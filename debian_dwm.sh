3253de6ed4f36e4e1caa1ab0bc2ee0583fd8fa6a
Orion <orioncrocker@gmail.com>
2020-07-28 22:10:06 -0700

    sudo bugfix, almost all important commands use sudo now

--- debian_dwm.sh
+++ debian_dwm.sh
@@ -8,42 +8,42 @@ if [ ! "$EUID" -ne 0 ]
 fi
 
 # apt install all essential packages
-sudo apt install -y rsync acpi build-essential wget curl alsa-utils wicd-curses xinit libx11-dev libxft-dev libxinerama-dev xclip xvkbd feh figlet
+sudo apt install -y rsync acpi build-essential sudo wget curl alsa-utils wicd-curses xinit libx11-dev libxft-dev libxinerama-dev xclip xvkbd feh figlet
 
-#wget all packages
+#sudo wget all packages
 if [ -d "packages" ]
 then
     sudo rm -rf packages
 fi
 mkdir packages
 cd packages
-wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
-wget https://dl.suckless.org/st/st-0.8.1.tar.gz
-wget https://dl.suckless.org/tools/dmenu-4.9.tar.gz
+sudo wget https://dl.suckless.org/dwm/dwm-6.2.sudo tar.gz
+sudo wget https://dl.suckless.org/st/st-0.8.1.sudo tar.gz
+sudo wget https://dl.suckless.org/tools/dmenu-4.9.sudo tar.gz
 
 # install dwm
-tar -xvf dwm*
+sudo tar -xvf dwm*
 cd dwm-6.2
 make
 sudo make install
 cd ..
 
 # install st
-tar -xvf st*
+sudo tar -xvf st*
 cd st-0.8.1
 make
 sudo make install
 cd ..
 
 # install dmenu
-tar -xvf dmenu*
+sudo tar -xvf dmenu*
 cd dmenu-4.9
 make
 sudo make install
 cd ..
 
 # cleaning up the mess
-sudo rm -rf *.tar.gz
+sudo rm -rf *.sudo tar.gz
 cd ..
 
 # update xorg.conf.d for backlight and mouse functionality
@@ -64,4 +64,4 @@ rsync -r images ~/
 
 # done!
 figlet done!
-echo "type 'startx' to enter dwm environment"
+echo "type 'ssudo tartx' to enter dwm environment"
